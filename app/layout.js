ba44597a997f1ded03b24c1b6a1f9865fc76dae2
ahmed iblao <ahmediblao53@gmail.com>
2024-01-04 23:58:56 +0300

    fixing bugs

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Link from 'next/link'
-import Image from 'next/image'
-import img from './vlogo.png'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,15 +13,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <div className='flex fixed mb-40 flex-col justify-start items-center gap-3 bg-[#ff8800] h-[100vh] w-56'>
-        <div >
-        <Image src={img} alt="Picture of the logo" />
-        </div>
+      <div className='flex justify-center items-center gap-5 bg-gradient-to-br
+       from-transparent text-white to-[#448585] h-14'>
       <Link href="/">Home</Link>
       <Link href="/posts">Posts</Link>
-      <Link href="/">Articales</Link>
-      <Link href="/">post</Link>
-      <Link href="/">profile</Link>
+      <Link href="/articales">Articales</Link>
       </div>
       {children}
       </body>
