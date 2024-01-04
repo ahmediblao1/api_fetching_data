89ae633c6d93bb71dde89f3987786a232c0f2ca3
ahmed iblao <ahmediblao53@gmail.com>
2024-01-04 23:57:39 +0300

    fixing bugs

--- app/articales/[title]/page.jsx
+++ app/articales/[title]/page.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function comingArticales(props) {
     console.log(props)
     return (
-        <div className='flex justify-center items-center font-bold mt-5'>
+        <div className='flex justify-center items-center font-bold '>
         <p>comingArticales</p>
         <p className='font-thin mt-5'>{props.params.title}</p>
         </div>
