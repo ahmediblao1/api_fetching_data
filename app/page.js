cbb8a19bdc9eb66b03095698c40877a2e4fb5c74
ahmed iblao <ahmediblao53@gmail.com>
2024-01-04 23:37:48 +0300

    fixing bugs

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='flex justify-center items-center font-bold  '>
+    <div className='flex justify-center items-center font-bold mt-5 '>
       <p>Home</p>
     </div>
   )
